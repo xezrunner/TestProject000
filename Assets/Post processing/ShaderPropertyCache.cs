cba7e9bf62bc2c85feeccb291558149e17e07245
Sebastian Kassai <8061077+xezrunner@users.noreply.github.com>
2024-08-16 02:13:26 +0200

    ShaderPropCache: fix typo

--- Assets/Post processing/ShaderPropertyCache.cs
+++ Assets/Post processing/ShaderPropertyCache.cs
@@ -77,7 +77,7 @@ public static class ShaderPropertyCache {
         // In non-editor builds, the dataPath is <proj>/Build/<proj>_Data - we have to go up one more:
         // TODO: This is totally busted on macOS:
         //string projectName = projectPathTokens[^3];
-        string projectName = "TestProject000" // TEMP: workaround
+        string projectName = "TestProject000"; // TEMP: workaround
 #endif
         return projectName;
     }
