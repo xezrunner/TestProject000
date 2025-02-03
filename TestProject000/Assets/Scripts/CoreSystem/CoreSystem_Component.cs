57f4b212a1e454d1c1151257884f6d45ae8d1a93
xezrunner <8061077+xezrunner@users.noreply.github.com>
2025-02-03 20:14:24 +0100

    proj: misc

--- TestProject000/Assets/Scripts/CoreSystem/CoreSystem_Component.cs
+++ TestProject000/Assets/Scripts/CoreSystem/CoreSystem_Component.cs
@@ -20,7 +20,7 @@ namespace CoreSystem {
             StartupShell?.STSHELL_SetActive(CORESYSTEM_STARTUP_OPTS.isFullStartup);
 
             if (SceneManager.GetActiveScene().name == CORESYSTEM_SCENE_NAME && !Camera.main) { // TEMP:
-                var obj = new GameObject();
+                var obj = new GameObject("Temporary camera (dev)");
                 var cam = obj.AddComponent<Camera>();
                 cam.clearFlags = CameraClearFlags.Color;
                 cam.backgroundColor = new(0.05f,0.05f,0.05f,1f);
