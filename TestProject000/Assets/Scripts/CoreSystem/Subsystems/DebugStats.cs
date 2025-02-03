461e3a1c33e09368f3df17589438210abd4549df
Sebastian Kassai <8061077+xezrunner@users.noreply.github.com>
2025-02-03 03:00:00 +0100

    core: DebugStats bringup

---
+++ TestProject000/Assets/Scripts/CoreSystem/Subsystems/DebugStats.cs
@@ -0,0 +1,189 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+namespace CoreSystem {
+
+    public class DebugStats : MonoBehaviour {
+        [Header("Components")]
+        [SerializeField] TMP_Text      statsTextCom;
+
+        [SerializeField] RectTransform quicklinesContainer;
+        [SerializeField] GameObject    quicklineTextPreset; // Whatever is set as the preset will be removed at runtime!
+
+        [Header("Configuration")]
+        [SerializeField] float quicklineTimeoutSec = 5f;
+
+        void Awake() {
+            if (!statsTextCom) {
+                Debug.LogWarning("[debugstats] No TextMeshPro component assigned!");
+                statsTextCom = GetComponent<TMP_Text>();
+                if (!statsTextCom) Debug.LogWarning("[debugstats] No TextMeshPro component found!");
+            }
+            if (!quicklinesContainer) Debug.LogWarning("[debugstats] No TextMeshPro component assigned!");
+
+            // Setup quickline text preset:
+            if (quicklineTextPreset) {
+                var obj = Instantiate(quicklineTextPreset);
+                var com = obj.GetComponent<TextMeshProUGUI>();
+                if (!com) Debug.LogError("QL Prefab exists, but no TMP_Text was found on it!");
+                com.SetText((string)null);
+
+                Destroy(quicklineTextPreset);
+                quicklineTextPreset = obj; // To preserve the above changes, since we deviate in the editor for previewing purposes
+            }
+
+            createQuicklines();
+        }
+
+        void OnEnable() {
+            Application.logMessageReceived += UNITY_logMessageReceived;
+        }
+        void OnDisable() {
+            Application.logMessageReceived -= UNITY_logMessageReceived;
+        }
+        
+        class QuicklineInfo {
+            public QuicklineInfo() { }
+            public QuicklineInfo(TMP_Text textCom) => this.textCom = textCom;
+            
+            public bool     isRetired = true;
+            public float    timestamp;
+            public TMP_Text textCom;
+
+            public void retire() {
+                isRetired = true;
+                textCom?.SetText((string)null);
+            }
+        }
+
+        static int QUICKLINES_COUNT = 8;
+        QuicklineInfo[] quicklines = new QuicklineInfo[QUICKLINES_COUNT];
+
+        void createQuicklines() {
+            for (int i = 0; i < QUICKLINES_COUNT; ++i) {
+                var newLine = _quicklineCreateNewLine();
+                quicklines[i] = new(newLine.com);
+            }
+        }
+
+        void resizeQuicklines(int newCount) {
+            var newQuicklines = new QuicklineInfo[newCount];
+            for (int i = 0; i < newCount; ++i) {
+                newQuicklines[i] = new();
+
+                if (i < quicklines.Length) {
+                    newQuicklines[i].isRetired = quicklines[i].isRetired;
+                    newQuicklines[i].textCom   = quicklines[i].textCom;
+                    newQuicklines[i].timestamp = quicklines[i].timestamp;
+                } else {
+                    newQuicklines[i].textCom = _quicklineCreateNewLine().com;
+                }
+            }
+
+            if (newCount < QUICKLINES_COUNT) {
+                for (int i = QUICKLINES_COUNT - 1; i >= newCount; --i) {
+                    Destroy(quicklines[i].textCom.gameObject);
+                }
+            }
+
+            QUICKLINES_COUNT = newCount;
+            quicklines = newQuicklines; // TODO: leak?            
+        }
+
+        (GameObject obj, TMP_Text com) _quicklineCreateNewLine() {
+            GameObject obj;
+            TMP_Text   com;
+            
+            if (quicklineTextPreset) {
+                obj = Instantiate(quicklineTextPreset);
+                com = obj.GetComponent<TextMeshProUGUI>();
+                if (!com) Debug.LogError("QL Prefab exists, but no TMP_Text was found on it!");
+            } else {
+                obj = new("Quickline");
+                com = obj.AddComponent<TextMeshProUGUI>();
+                //var rectTrans = obj.GetComponent<RectTransform>();
+            }
+
+            obj.transform.SetParent(quicklinesContainer);
+
+            return (obj, com);
+        }
+
+        void quicklinePush(string text) {
+            // Find available QL line:
+            QuicklineInfo target = null;
+            for (int i = 0; i < QUICKLINES_COUNT; ++i) {
+                var line = quicklines[ i];
+
+                var isFree = true;
+                if (line.textCom == null) {
+                    line.textCom = _quicklineCreateNewLine().com;
+                    target = line; break;
+                }
+                if (line.isRetired) {
+                    target = line; break;
+                }
+                // if (!line.textCom.text.IsEmpty())                     isFree = false;
+                if (Time.time < line.timestamp + quicklineTimeoutSec) isFree = false;
+
+                if (isFree) {
+                    target = line; break;
+                }
+            }
+
+            // If no free target was found, shift everything up by 1 and use last line:
+            if (target == null) {
+                for (int i = 0; i < QUICKLINES_COUNT - 1; ++i) {
+                    QuicklineInfo a, b;
+                    a = quicklines[i];
+                    b = quicklines[i + 1];
+
+                    a.textCom.SetText(b.textCom.text);
+                    a.timestamp = b.timestamp;
+                }
+                target = quicklines[QUICKLINES_COUNT - 1];
+            }
+
+            target.timestamp = Time.time;
+            target.isRetired = false;
+            target.textCom.SetText(text);
+        }
+
+        // TODO: we may want to read/receive messages from DebugConsole instead:
+        static bool UNITY_RedirectLogMessages = true;
+        void UNITY_logMessageReceived(string text, string stackTrace, LogType level) {
+            if (!UNITY_RedirectLogMessages) return;
+            
+            if      (level == LogType.Warning) text = $"<color=#FB8C00>{text}</color>";
+            else if (level == LogType.Error)   text = $"<color=#EF5350>{text}</color>";
+
+            quicklinePush(text);
+        }
+
+        public static void debugLog_noHandle(string text) {
+            var before = UNITY_RedirectLogMessages;
+            UNITY_RedirectLogMessages = false;
+            Debug.Log(text);
+            UNITY_RedirectLogMessages = before;
+        }
+
+        void LateUpdate() {
+            for (int i = 0; i < QUICKLINES_COUNT; ++i) {
+                var line = quicklines[i];
+                if (!line.isRetired) {
+                    if (Time.time < line.timestamp + quicklineTimeoutSec) continue;
+                    line.retire();
+                }
+            }
+
+            if (Keyboard.current.pKey.wasPressedThisFrame) quicklinePush   ($"Test       {Time.time}");
+            if (Keyboard.current.oKey.wasPressedThisFrame) Debug.LogWarning($"Warn Test  {Time.time}");
+            if (Keyboard.current.iKey.wasPressedThisFrame) Debug.LogError  ($"Err  Test  {Time.time}");
+            
+            if (Keyboard.current.nKey.wasPressedThisFrame) resizeQuicklines(6);
+            if (Keyboard.current.mKey.wasPressedThisFrame) resizeQuicklines(30);
+        }
+
+    }
+}
