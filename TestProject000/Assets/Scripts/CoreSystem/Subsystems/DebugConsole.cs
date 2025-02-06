6d4d69f035f2064ff2b690d9b1f7d3888e37afe4
Sebastian Kassai <8061077+xezrunner@users.noreply.github.com>
2025-02-06 06:52:31 +0100

    console: filtering

--- TestProject000/Assets/Scripts/CoreSystem/Subsystems/DebugConsole.cs
+++ TestProject000/Assets/Scripts/CoreSystem/Subsystems/DebugConsole.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace CoreSystem {
-    
+
+    [Flags] public enum LogCategory: uint {
+        Unknown    = 0,
+        Unity      = 1 << 0,
+        CoreSystem = 1 << 1,
+        Project    = 1 << 2,
+        External   = 1 << 3,
+    }
+
     public class DebugConsole : MonoBehaviour {
         [Header("Components")]
         [SerializeField] RectTransform canvasRectTrans;
@@ -17,7 +27,7 @@ namespace CoreSystem {
         
         [SerializeField] GameObject consoleOutputTextPreset;
 
-        [SerializeField] TMP_Text scrollDebugTextCom;
+        [SerializeField] TMP_Text debugTextCom;
 
         [Header("Settings")]
         [SerializeField] Vector2 textPadding = new(24, 16);
@@ -30,8 +40,6 @@ namespace CoreSystem {
         void Awake() {
             if (!selfRectTrans)   selfRectTrans   = GetComponent<RectTransform>();
             if (!canvasRectTrans) canvasRectTrans = selfRectTrans.parent.GetComponent<RectTransform>();
-            
-            consoleOutput = new(capacity: 300);
 
             createConsoleLines();
         }
@@ -43,7 +51,64 @@ namespace CoreSystem {
             Application.logMessageReceived -= UNITY_logMessageReceived;
         }
 
-        public List<string> consoleOutput; // All messages
+        void setState() {
+            // ...
+            
+            updateConsoleFiltering();
+        }
+
+        public class ConsoleLineInfo {
+            public LogCategory category;
+            // TODO: 
+            // log level?
+            // caller member info?
+            // stack trace?
+            public string text;
+        }
+
+        int consoleOutputCount;
+        List<ConsoleLineInfo> consoleOutput = new(capacity: 500);
+
+        LogCategory consoleFilterFlags = LogCategory.Unity | LogCategory.CoreSystem;
+        public const LogCategory CONSOLEFILTERFLAGS_ALL = (LogCategory)uint.MaxValue;
+
+        int consoleOutputFilteredCount = 0;
+        List<ConsoleLineInfo> consoleOutputFiltered = new(capacity: 500);
+
+        void pushText(LogCategory category, string text) {
+            var info = new ConsoleLineInfo() {
+                category = category,
+                text     = text
+            };
+            consoleOutputCount += 1;
+            consoleOutput.Add(info);
+
+            // TODO: If the console is visible:
+            if (true) {
+                updateConsoleFiltering();
+                updateLines();
+                scroll(SCROLL_BOTTOM);
+            }
+        }
+
+        void updateConsoleFiltering() {
+            if ((consoleFilterFlags & CONSOLEFILTERFLAGS_ALL) == CONSOLEFILTERFLAGS_ALL) {
+                consoleOutputFiltered = consoleOutput;
+                consoleOutputFilteredCount = consoleOutputCount;
+                return;
+            }
+
+            // TODO: @Performance
+            consoleOutputFiltered = consoleOutput.Where(x => consoleFilterFlags.HasFlag(x.category)).ToList();
+            consoleOutputFilteredCount = consoleOutputFiltered.Count;
+        }
+
+        void setConsoleFilterFlags(LogCategory flags) {
+            if (flags == consoleFilterFlags) return;
+            consoleFilterFlags = flags;
+
+            updateConsoleFiltering();
+        }
 
         public static bool UNITY_RedirectLogMessages = true;
         void UNITY_logMessageReceived(string text, string stackTrace, LogType level) {
@@ -52,14 +117,7 @@ namespace CoreSystem {
             if      (level == LogType.Warning) text = $"<color=#FB8C00>{text}</color>";
             else if (level == LogType.Error)   text = $"<color=#EF5350>{text}</color>";
 
-            consoleOutput.Add(text);
-            updateLines();
-
-            scroll(SCROLL_BOTTOM);
-        }
-        
-        void pushText(string text) {
-
+            pushText(LogCategory.Unity, text);
         }
 
         // UGUI has the scrolling T flipped:
@@ -101,12 +159,11 @@ namespace CoreSystem {
             // This function does "virtualized scrolling", where only the visible UI lines are updated with the console log output.
             // This results in much better performance, compared to keeping the whole log output within the console.
 
-            // Set scroll content height based on how many total log lines there are:
-            var fullHeight = (consoleOutput.Count * uiLineHeight) + (textPadding.y * 2f);
-            scrollContentRectTrans.sizeDelta = new(scrollContentRectTrans.sizeDelta.x, fullHeight);
-            
+            // Set scroll content height based on how many log lines there are:
+            var contentHeight = (consoleOutputFilteredCount * uiLineHeight) + (textPadding.y * 2f);
+
             // NOTE: despite UGUI's flipped nature, this is [0-1, top-bottom] here:
-            var scroll = (consoleOutput.Count <= uiLineCount) ? 
+            var scroll = (consoleOutputFilteredCount <= uiLineCount) ? 
                         // If we scroll when scrolling shouldn't be possible (content fits on screen), Unity will claim 
                         // that we scrolled to the bottom (1) for some reason.
                         // We'll limit this here such that we always remain scrolled to the top when this is the case.
@@ -115,19 +172,35 @@ namespace CoreSystem {
                         // BUG: UGUI sometimes gives us a scrolling value of like 1.0000014, which if we subtract,
                         // will obviously give us a negative number. So let's clamp it:
                         Mathf.Clamp01(SCROLL_TOP - scrollRect.verticalNormalizedPosition);
+            // Alternatively (potentially more accurate?)
+            // scroll = (consoleOutputFilteredCount <= uiLineCount) ? SCROLL_BOTTOM : Mathf.Clamp01(scrollContentRectTrans.anchoredPosition.y / scrollRectTrans.rect.height);
 
-            // This is the index into the total console log output lines (as opposed to the constant few visible UI lines).
+            // This is the index into the console log output lines (as opposed to the constant few visible UI lines).
             // We want this index to be offset by the visible UI line count when scrolling is possible, since we want the
             // log end with the last UI line.
             // NOTE: take into consideration the extra UI line that exists (+1), versus what we want to see in the UI.
             // The extra is used for smooth scrolling.
-            var indexIntoOutput = Mathf.FloorToInt(scroll * Mathf.Min(a: consoleOutput.Count,
-                                                                      b: consoleOutput.Count - (uiLineCount - extraLineCountForSmoothScrolling)));
+            var indexIntoOutput = Mathf.FloorToInt(scroll * Mathf.Min(a: consoleOutputFilteredCount,
+                                                                      b: consoleOutputFilteredCount - (uiLineCount - extraLineCountForSmoothScrolling)));
 
-            scrollDebugTextCom.SetText($"output lines: {consoleOutput.Count} | scroll: {scroll:N2}  height: {fullHeight:N3}  indexIntoOutput: {indexIntoOutput}");
+            debugTextCom?.SetText($"total lines: {consoleOutputCount}  filtered lines: {consoleOutputFilteredCount} | "     + 
+                                  $"scroll: {scroll:N2}  height: {contentHeight:N3}  indexIntoOutput: {indexIntoOutput} | " +
+                                  $"filter: [{consoleFilterFlags}]");
 
             for (int i = 0 ; i < uiLineCount; ++i) {
                 var uiLine = uiLines[i];
+
+                if (indexIntoOutput + i >= consoleOutputFilteredCount) {
+                    // Deactivate invisible lines
+                    uiLine.obj.SetActive(false);
+                    continue;
+                }
+
+                // Write the appropriate log index into the line:
+                var outputLine = consoleOutputFiltered[indexIntoOutput + i];
+                uiLine.obj.SetActive(true);
+                //line.com.SetText($"visual line index {i:D2}  text output index: {(indexIntoOutput + i):D3} | {outputLine}");
+                uiLine.com.SetText(outputLine.text);
 
                 // Position line within the scroll content area to the position where it should be:
                 // It is intentional that this is an "integer" that only updates when we scroll one line's worth of space.
@@ -138,22 +211,13 @@ namespace CoreSystem {
                 // at the top and bottom of the console.
                 // Perhaps we'd need to take the padding into consideration when creating the extra lines, to determine the amount
                 // of extra lines to create.
-                targetPos += textPadding;
+                targetPos   += textPadding;
                 targetPos.y *= -1; // UI has top at 1, so let's flip our logical calculations from above into that.
 
                 uiLine.com.rectTransform.anchoredPosition = targetPos;
-                
-                if (indexIntoOutput + i < consoleOutput.Count) {
-                    // Write the appropriate log index into the line:
-                    var outputLine = consoleOutput[indexIntoOutput + i];
-                    uiLine.obj.SetActive(true);
-                    //line.com.SetText($"visual line index {i:D2}  text output index: {(indexIntoOutput + i):D3} | {outputLine}");
-                    uiLine.com.SetText(outputLine);
-                } else {
-                    // Deactivate invisible lines
-                    uiLine.obj.SetActive(false);
-                }
             }
+
+            scrollContentRectTrans.sizeDelta = new(scrollContentRectTrans.sizeDelta.x, contentHeight);
         }
 
         public void OnValueChanged(Vector2 v) {
@@ -167,22 +231,42 @@ namespace CoreSystem {
             scrollTarget = t;
         }
 
-        float t = 0;
+        float t;
         void LateUpdate() {
             if (scrollTarget != -1f) {
                 scrollRect.verticalNormalizedPosition = scrollTarget;
                 scrollTarget = -1f;
             }
 
-            if (Keyboard.current.shiftKey.isPressed && Keyboard.current.enterKey.wasReleasedThisFrame) {
-                for (int i = 0; i < 1000; ++i) Debug.Log(i);
+            if (Keyboard.current.shiftKey.isPressed && Keyboard.current.enterKey.wasPressedThisFrame) {
+                for (int i = 0; i < 30; ++i) {
+                    pushText(LogCategory.CoreSystem, $"This is a log entry that belongs to the CoreSystem log category. {i}");
+                    Debug.LogWarning($"This is a log entry that belongs to the Unity log category. {i}");
+                }
             }
 
             if (Keyboard.current.spaceKey.wasReleasedThisFrame) {
                 float targetHeight = selfRectTrans.sizeDelta.y == 300f ? 500f : selfRectTrans.sizeDelta.y == 500f ? canvasRectTrans.sizeDelta.y : 300f;
                 resizeConsole(targetHeight);
             }
+
+            if (Keyboard.current.tabKey.wasReleasedThisFrame) {
+                updateLines();
+            }
+
+            if (Keyboard.current.qKey.wasPressedThisFrame) {
+                setConsoleFilterFlags(consoleFilterFlags ^ LogCategory.Unity);
+                updateLines();
+            }
+            if (Keyboard.current.wKey.wasPressedThisFrame) {
+                setConsoleFilterFlags(consoleFilterFlags ^ LogCategory.CoreSystem);
+                updateLines();
+            }
+            if (Keyboard.current.eKey.wasPressedThisFrame) {
+                setConsoleFilterFlags(consoleFilterFlags == CONSOLEFILTERFLAGS_ALL ? LogCategory.Unknown : CONSOLEFILTERFLAGS_ALL);
+                updateLines();
+            }
         }
     }
-    
+
 }
